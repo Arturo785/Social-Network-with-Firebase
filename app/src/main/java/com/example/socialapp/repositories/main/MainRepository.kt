9668fccb3360883086150f361e024c8d431155dd
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2021-03-11 18:02:37 -0600

    Addition of post repository logic and crop logic

---
+++ app/src/main/java/com/example/socialapp/repositories/main/MainRepository.kt
@@ -0,0 +1,21 @@
+package com.example.socialapp.repositories.main
+
+import android.net.Uri
+import com.example.socialapp.data.entities.Post
+import com.example.socialapp.data.entities.User
+import com.example.socialapp.other.Resource
+
+interface MainRepository {
+
+    suspend fun createPost(imageUri : Uri, text : String) : Resource<Any>
+
+    suspend fun getUsers(uids : List<String>) : Resource<List<User>>
+
+    suspend fun getUser(uid : String) : Resource<User>
+
+    suspend fun getPostForFollows() : Resource<List<Post>>
+
+    suspend fun toggleLikeForPost(post: Post) : Resource<Boolean>
+
+    suspend fun deletePost(post: Post) : Resource<Post>
+}
